90f81c17216ab1607a01afef5f1aa761887ef7c8
remixproddev <officialremixmedia@gmail.com>
2026-09-12 11:17:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
+importScripts('/OneSignalSDKWorker.js');
+
 const CACHE_NAME = 'rewatch-cache-v1';
 const urlsToCache = ['/'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
+
+self.addEventListener('install', e => { 
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); 
+});
+
+self.addEventListener('fetch', e => { 
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); 
+});
